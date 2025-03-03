e2e0693427f66099eeee60c8bf13b0db0abb8c22
Bhavishy2801 <bhavishyrocker2801@gmail.com>
2025-03-03 22:10:52 +0530

    Some major changes

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ vid.playbackRate = 2;
 document.getElementById('overlayImage').addEventListener('click', function() {
     document.getElementById('myVideo').play();
   });
+
+  document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('active');
+  }); 
