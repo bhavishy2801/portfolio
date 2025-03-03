f0453ada3e4c69ec039dc46bd3f123e23faaf180
Bhavishy2801 <bhavishyrocker2801@gmail.com>
2025-03-03 16:19:45 +0530

    Some major changes

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let vid = document.getElementById("myVideo");
 vid.playbackRate = 2;
+
+document.getElementById('overlayImage').addEventListener('click', function() {
+    document.getElementById('myVideo').play();
+  });
