bcf12535831c657f57229037bd284693f406dcdc
Bhavishy2801 <bhavishyrocker2801@gmail.com>
2025-03-03 14:33:12 +0530

    Some major changes

--- script.js
+++
@@ -1,4 +0,0 @@
-// For future use if you want to add a mobile menu toggle or other interactive features.
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website is ready!");
-});
